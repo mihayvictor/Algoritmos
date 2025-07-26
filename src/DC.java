d19d6a2e566edceecde1a0c3174a41d3161c9460
mihayvictor <m.victorcezario@gmail.com>
2025-07-26 01:16:46 -0300

    Problema quadrado máximo

---
+++ src/DC.java
@@ -0,0 +1,34 @@
+
+
+public class DC {
+public static void main(String[] args) {
+    int largura = 1680, comprimento=640;
+
+    int[] result = encontrarQuadrado(largura, comprimento);
+    System.out.println();
+    System.out.println(formatarResultado(result));
+
+}
+
+public static int[] encontrarQuadrado(int x, int y){
+    
+    if (x == y) { //caso base
+        int[] result = {x , y};
+        return result;
+
+    }else if(x > y ){ //caso recurssivo  
+        x -= y; 
+        System.out.println("Reduzindo largura:     " +  x); 
+    }else{
+        y -= x;
+        System.out.println("Reduzindo comprimento: " + y);
+        }
+
+     return encontrarQuadrado(x, y);
+}
+
+public static String formatarResultado(int[] result){
+    return "Quadrado máximo:  " + result[0] + " x " + result[1];
+}
+
+}
