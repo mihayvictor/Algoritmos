c974007fefb8e67ea85e2f4c5f5afd18ac42fad1
mihayvictor <m.victorcezario@gmail.com>
2025-08-21 22:47:25 -0300

    Ordenação por seleção atualizada

--- src/OrdenacaoPorSelecao.java
+++ src/OrdenacaoPorSelecao.java
@@ -4,44 +4,44 @@ import java.util.List;
 public class OrdenacaoPorSelecao {
 
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<>();
-        arr.add(5);
-        arr.add(3);
-        arr.add(6);
-        arr.add(2);
-        arr.add(10);
+        List<Integer> lista = new ArrayList<>();
+        lista.add(5);
+        lista.add(3);
+        lista.add(6);
+        lista.add(2);
+        lista.add(10);
 
-        ordenacaoPorSelecao(arr);
-
+        System.out.println("Lista não ordenada: " + lista);
+        System.out.println("Lista ordenada: " + ordenacaoPorSelecao(lista));
+    
     }
     
-    public static int buscarMenor(List<Integer> arr){
-        int menor = arr.get(0);
+    public static int buscarMenor(List<Integer> lista){
+        int menor = lista.get(0);
         int menor_indice = 0;
 
-        for (int i = 1; i < arr.size(); i++){
+        for (int i = 1; i < lista.size(); i++){
 
-            if (arr.get(i) < menor) {
-                menor = arr.get(i);
+            if (lista.get(i) < menor) {
+                menor = lista.get(i);
                 menor_indice = i;
             }
         }
         return menor_indice;
     }
 
-    public static List<Integer> ordenacaoPorSelecao(List<Integer> arr){
-        int menor = arr.get(0);
+    public static List<Integer> ordenacaoPorSelecao(List<Integer> lista){
+        int menor = lista.get(0);
         int menor_indice = 0;
-        List<Integer> novoArr = new ArrayList<>();
+        List<Integer> novaLista = new ArrayList<>();
 
-        while (!arr.isEmpty()) {
-            menor_indice = buscarMenor(arr);
-            menor = arr.get(menor_indice);
-            novoArr.add(menor);
-            arr.remove(menor_indice);
+        while (!lista.isEmpty()) {
+            menor_indice = buscarMenor(lista);
+            menor = lista.get(menor_indice);
+            novaLista.add(menor);
+            lista.remove(menor_indice);
         }
-        System.out.println(novoArr);
-        return novoArr;
+        return novaLista;
     }
 
 
