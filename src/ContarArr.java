73c47c0bbd6bb8250ce90b8bd0835323cffbf4b1
mihayvictor <m.victorcezario@gmail.com>
2025-08-31 22:27:42 -0300

    Função max e soma de valores no array

--- src/ContarArr.java
+++ src/ContarArr.java
@@ -1,9 +1,13 @@
 public class ContarArr {
     public static void main(String[] args) {
-        int[] arr = {2,4,6};
+        int[] arr = {2,10,4,6};
         
-        int total = contar(arr, 0);
-        System.out.println("Quantidade de itens no array: " +total); 
+        int totalI = contar(arr, 0);
+        System.out.println("Quantidade de itens no array: " +totalI); 
+        int maior = max(arr, 0,0);
+        System.out.println("Maior valor: " + maior);
+        int totalSoma = soma(arr, 0, 0);
+        System.out.println("Total valores somados: " + totalSoma);
     }
 
     static int contar(int[]arr, int i){
@@ -12,5 +16,32 @@ public class ContarArr {
         }
         i++;
         return contar(arr, i);     
+    }
+
+    static int max(int[]arr, int i, int maior){
+        if (i == 0) {
+            i++;
+            maior = arr[0];
+            return max(arr, i, maior);
+        }
+        if (i == arr.length) {
+            return maior;
+        }
+        else {
+            if(maior<arr[i]) {
+            maior = arr[i];
+            }
+        i++;
+        return max(arr, i, maior);
+        } 
+    }
+
+    static int soma(int[] arr, int i, int total){
+        if (i == arr.length) {
+            return total;
+        }
+        total += arr[i];
+        i++;
+        return soma(arr, i, total);
     }
 }
